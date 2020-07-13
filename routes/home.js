df692fcc49105fb19fc612b0f6e08c3c4c4c0100
yatharth <yatharthverma070@gmail.com>
2020-07-13 19:26:52 +0530

    :bookmark: - Add post route

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const showdown = require('showdown'); 
 
 router.get('/', (req,res) => {
     res.render('home');
+})
+
+router.post('/blog', (req,res) => {
+    console.log(req);
+    var converter = new showdown.Converter();
+    var md = req.body.blogBody;
+    var html = converter.makeHtml(md);
+    res.send(html);
 })
 
 module.exports = router;
