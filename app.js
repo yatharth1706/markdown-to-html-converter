b48452993cafede0f1b26c6c074cd2acac147212
yatharth <yatharthverma070@gmail.com>
2020-07-13 19:28:36 +0530

    :rocket: - Configuration

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // static files configuration
 app.use(express.static(path.join(__dirname, 'public')));
