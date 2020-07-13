a2e0ba7b6dabfdd160e19d28e818d5e5f0e1ab65
yatharth <yatharthverma070@gmail.com>
2020-07-13 18:13:04 +0530

    :zap: - Server setup

---
+++ app.js
@@ -0,0 +1,22 @@
+/** Basic server setup */
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+
+// static files configuration
+app.use(express.static(path.join(__dirname, 'public')));
+
+// handlebars configuration
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/home'));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
